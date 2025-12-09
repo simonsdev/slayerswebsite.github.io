f499890f82d0b7fd02b01445e0cac52997f328f8
BMPRS <83304358+BMPRS@users.noreply.github.com>
2025-12-09 14:57:36 +0000

    Add files via upload

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,18 +1,20 @@
 function validation() {
-	let valid = true;
+	
 	let F = document.getElementById("fname").value.trim()
 	let L = document.getElementById("lname").value.trim()
 	let E = document.getElementById("email").value.trim()
-	if (F=="" || L == "" || E == ""){
+	
+	if (F ==="" || L === "" || E === ""){
 		alert("Please fill in all required fields");
-		valid = false;	
+		return false;	
 	}
 	
 	let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	if (!emailRegex.test(email)) {
+	
+	if (!emailRegex.test(E)) {
 		alert("Please enter a valid email address");
-		valid = false;
+		return false;
 	}
 
-	return valid;
+	return true;
 }
